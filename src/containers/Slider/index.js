0387f5668d0d3dc136bc23921d7eee239aded63b
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-08-28 11:47:51 +0200

    fix: Trier les événements par date en ordre décroissant

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,24 +1,37 @@
+// Importation des hooks et dépendances nécessaires
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
+// Définition du composant Slider
 const Slider = () => {
   const { data } = useData();
+
+   // Récupération des données depuis le contexte global
   const [index, setIndex] = useState(0);
+
+    // Triage des événements par date en ordre décroissant
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
+
+   // Fonction pour avancer au slide suivant ou revenir au premier
   const nextCard = () => {
     setTimeout(
       () => setIndex(index < byDateDesc.length ? index + 1 : 0),
       5000
     );
   };
+
+  
+  // Exécute la fonction nextCard après chaque rendu du composant
   useEffect(() => {
     nextCard();
   });
+
+  // Rendu du slider avec ses slides et la pagination
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
