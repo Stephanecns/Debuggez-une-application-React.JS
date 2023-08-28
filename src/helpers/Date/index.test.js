88c8d1f1a7f729cd8efc5065cf7ebaea963a9cc0
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-08-28 22:42:56 +0200

    Ajout de tests unitaires pour la fonction getMonth et documentation

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,22 @@
-/**
- * 
- */
+// Importation de notre fonction "getMonth" pour tester.
+import { getMonth } from "../../helpers/Date";
 
 describe("Date helper", () => {
+      // Ce groupe de tests est spécifique à la fonction "getMonth".
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+
+            // On convertit la chaîne de caractères en un objet Date.
+            const dateObj = new Date("2022-01-01");
+            // On appelle notre fonction avec cet objet
+            const result = getMonth(dateObj);
+             // On vérifie que le résultat est bien "janvier".
+            expect(result).toEqual("janvier");
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const dateObj = new Date("2022-07-08");
+            const result = getMonth(dateObj);
+            expect(result).toEqual("juillet");
         });
     });
 })
-
