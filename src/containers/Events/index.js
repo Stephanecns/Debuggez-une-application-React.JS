6d291fa0c199644756bd86b379cd1f9bd69c32e6
Stéphane <130861507+Stephanecns@users.noreply.github.com>
2023-08-30 19:44:54 +0200

    Ajout d'une condition pour gérer les valeurs nulles dans le filtre de type d'événement

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -7,12 +7,20 @@ import ModalEvent from "../ModalEvent";
 
 import "./style.css";
 
+// EventList est un composant React qui liste des événements.
+// Il utilise un filtre basé sur le type d'événement et une pagination pour afficher ces événements.
+
 const PER_PAGE = 9;
 
 const EventList = () => {
+   // Utilise le hook useData pour obtenir les données des événements et une éventuelle erreur.
   const { data, error } = useData();
-  const [type, setType] = useState();
+    // Gère le type d'événement actuellement sélectionné pour le filtrage. Initialisé à null (pas de filtrage).
+  const [type, setType] = useState(null);
+  // Gère la page actuelle de la pagination. Initialisé à 1 (première page).
   const [currentPage, setCurrentPage] = useState(1);
+  // Filtrer les événements en fonction du type sélectionné et de la page actuelle.
+  // Si type est undefined ou null, aucun filtrage n'est appliqué
   const filteredEvents = (
     (!type
       ? data?.events
@@ -22,15 +30,21 @@ const EventList = () => {
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
     ) {
-      return true;
+      // Ajouter un filtre sur les type d'événement
+      return type === undefined || event.type === type || type === null;
     }
     return false;
   });
+    // Modifie le type d'événement pour le filtrage et réinitialise la pagination.
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+  // Calcule le nombre total de pages en fonction du nombre d'événements filtrés.
+  // Utilise Math.floor pour obtenir un nombre entier, et ajoute 1 pour assurer qu'il y a au moins une page.
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+    // Crée un ensemble unique de tous les types d'événements disponibles dans les données.
+  // Ceci est utilisé pour alimenter le composant Select pour le filtrage par type.
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
